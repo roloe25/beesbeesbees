c6d1aa27f7eb9488a3e42c458e19985cc67fd411
Albert Jo <for.aljo@gmail.com>
2020-02-10 11:28:40 -0800

    should have commited more i know, next time

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  Grub.call(this)
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+//what does this bottom code do??!
+Bee.prototype.constructor = Bee;
 
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,20 @@
+
+
 var ForagerBee = function() {
+  Bee.call(this)
+  this.age = 10;
+  this.job = 'find pollen';
+  this.honeyPot = 0;
+  this.canFly = true;
+  this.treasureChest = [];
 };
+
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+//what does this bottom code do??!
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function (item) {
+this.treasureChest.push(item);
+}
 
